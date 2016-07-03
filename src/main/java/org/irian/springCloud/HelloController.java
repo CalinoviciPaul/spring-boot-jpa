0045b62c3349bd39c28ac567513dba005f51408e
Paul Calinovici <calinovicipaul@gmail.com>
2016-07-03 19:36:57 +0300

    added Thymeleaf engine page!

--- src/main/java/org/irian/springCloud/HelloController.java
+++ src/main/java/org/irian/springCloud/HelloController.java
@@ -1,8 +1,11 @@
 package org.irian.springCloud;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * Created by IrianLaptop on 7/3/2016.
@@ -10,8 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hi")
-    public @ResponseBody String hi(){
-        return "Hello World!";
+    /**
+     * Controller returns a view name
+     * The ViewResolver finds a matching template
+     * The renderer renders the template
+     */
+    @RequestMapping("/hi/{name}")
+    public  String hi(Map model, @PathVariable String name){
+        model.put("name",name);
+        return "hello";
     }
 }
