88e43bce246f225df2d6bf38b2b5d21f90a27d51
Paul Calinovici <calinovicipaul@gmail.com>
2016-07-03 19:14:31 +0300

    added some comments

--- src/main/java/org/irian/springCloud/SpringCloudApplication.java
+++ src/main/java/org/irian/springCloud/SpringCloudApplication.java
@@ -4,8 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * @SpringBootApplications is the same as:
+ * 	@Configuration - the class represents a configuration class
+ * 	@ComponentScan - scan for classes adnotated with @Component
+ * 	@EnableAutoConfiguration - create by default missing beans
+ */
 public class SpringCloudApplication {
 
+	/**
+	 * this launches SpringApplicationContext with the configuration class as parameter
+	 * */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudApplication.class, args);
 	}
